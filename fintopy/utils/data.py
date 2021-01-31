3d3eb605b72931c88c9dfc397dcc427c1b6eb95c
mbonix <bonifacio.marco@gmail.com>
2021-01-31 20:15:10 +0100

    Sync remote

--- fintopy/utils/data.py
+++ fintopy/utils/data.py
@@ -1,6 +1,6 @@
 from typing import List, Set, Tuple, Union
 from numpy import ndarray
-from pandas import Series
+from pandas import DataFrame, Index, MultiIndex, Series
 
 
 def bloomberg_isins_to_tickers(
@@ -27,10 +27,53 @@ def bloomberg_isins_to_tickers(
   return set([f'/ISIN/{isin}' for isin in isins])
   
   
-  def bbg_i2t(
-    isins: Union(List, Tuple, ndarray, Series)
-    ) -> Set:
-    """
-    Convenience shorthand for function `bloomberg_isins_to_tickers`
-    """
-    return bloomberg_isins_to_tickers(isins)
+def bbg_i2t(
+  isins: Union(List, Tuple, ndarray, Series)
+  ) -> Set:
+  """
+  Convenience shorthand for function `bloomberg_isins_to_tickers`.
+  """
+  return bloomberg_isins_to_tickers(isins)
+    
+    
+def bloomberg_tickers_to_isins(
+  tickers: Index
+  ) -> Index:
+  """
+  Returns ISIN codes deleting the Bloomberg request format.
+    
+  Parameters
+  ----------
+  tickers: Pandas Index (index or columns)
+  
+  Returns
+  -------
+  Pandas Index
+  """
+  return Index([ticker[6:] for ticker in tickers])
+  
+  
+def bbg_t2i(
+  tickers: Index
+  ) -> Index:
+  """
+  Convenience shorthand for function `bloomberg_tickers_to_isins`.
+  """
+  return bloomberg_tickers_to_isins(tickers)
+
+
+def bloomberg_dropfield(
+  columns: MultiIndex
+  ) -> Index
+  """
+  Drop the inner level from a Pandas two-level MultiIndex (for Bloomberg historical request with one field).
+  
+  Parameters
+  ----------
+  columns: Pandas MultiIndex
+  
+  Returns
+  -------
+  Pandas Index
+  """
+  return columns.droplevel[1]
