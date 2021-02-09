c0bd56602425f12a36c6861450126e608b013b8f
Marco Bonifacio <bonifacio.marco@gmail.com>
2021-02-09 15:27:44 +0100

    Import in main module

--- fintopy/__init__.py
+++ fintopy/__init__.py
@@ -1,2 +1,4 @@
+from fintopy.utils import *
+
 __version__ = '0.0.1'
 __author__ = 'Marco Bonifacio'
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,79 +2,71 @@ import numpy as np
 import pandas as pd
 import pytest
 
-import fintopy.utils.data as fud
-import tests.test_data as td
+import fintopy as fin
+import tests.data as td
 
 
-def test_bbg_i2t():
+def test_isins_to_tickers():
     isins_seq = td.isin_codes + ['FAKE']
     isins_array = np.asarray(isins_seq)
     isins_series = pd.Series(isins_seq, index=range(len(isins_seq)))
 
     # test isins as Sequence
-    result = fud.bbg_i2t(isins_seq)
+    result = fin.isins_to_tickers(isins_seq)
     expected = set(['/ISIN/IT1234567890', '/ISIN/DE1234567890'])
     assert result == expected
 
     # tests isins as ndarray
-    result = fud.bbg_i2t(isins_array)
+    result = fin.isins_to_tickers(isins_array)
     expected = set(['/ISIN/IT1234567890', '/ISIN/DE1234567890'])
     assert result == expected
 
     # tests isins as Series
-    result = fud.bbg_i2t(isins_series)
+    result = fin.isins_to_tickers(isins_series)
     expected = set(['/ISIN/IT1234567890', '/ISIN/DE1234567890'])
     assert result == expected
 
-    # raises an exception
-    with pytest.raises(Exception):
-        fud.bbg_i2t('123')
+    # raises a NotImplementedError
+    with pytest.raises(NotImplementedError):
+        fin.isins_to_tickers('123')
 
 
-def test_bbg_t2i():
+def test_tickers_to_isins():
     tickers = ['/ISIN/IT1234567890', '/ISIN/DE1234567890']
     tickers_idx = pd.Index(tickers)
 
     # tests tickers as Index
-    result = fud.bbg_t2i(tickers_idx)
+    result = fin.tickers_to_isins(tickers_idx)
     expected = pd.Index(['IT1234567890', 'DE1234567890'])
     assert result.all() == expected.all()
 
-    # raises an exception
-    with pytest.raises(Exception):
-        fud.bbg_t2i(tickers)
+    # raises a NotImplementedError
+    with pytest.raises(NotImplementedError):
+        fin.tickers_to_isins(tickers)
 
 
-def test_bloomberg_dropfield():
-    idx1 = ['IT1234567890', 'DE1234567890', 'US1234567890']
-    idx2 = 3 * ['PX_LAST']
-    idx = pd.MultiIndex.from_arrays([idx1, idx2], names=('isin', 'price'))
+def test_dropfield():
+    idx = td.px_2lvls_df.columns
 
     # tests MultiIndex
-    result = fud.bloomberg_dropfield(idx)
-    expected = pd.Index(idx1)
+    result = fin.dropfield(idx)
+    expected = pd.Index(td.isin_codes)
     assert result.all() == expected.all()
 
-    # raises an Exception
-    with pytest.raises(Exception):
-        fud.bloomberg_dropfield(pd.Index(idx1))
+    # raises a NotImplementedError
+    with pytest.raises(NotImplementedError):
+        fin.dropfield(pd.Index(td.isin_codes))
 
 
-def test_bloomberg_xs():
-    col1 = 2 * ['IT1234567890', 'DE1234567890', 'US1234567890']
-    col2 = 3 * ['PX_ASK', 'PX_BID']
-    col = pd.MultiIndex.from_arrays([col1, col2])
-    idx = pd.date_range('2021-01-01', '2021-01-15')
-    df = pd.DataFrame(index=idx, columns=col, data=1)
+def test_xs_list():
+    df = td.px_2lvls_multi_df
     
     # tests DataFrame
-    result = fud.bloomberg_xs(df)
-    for r in result:
-        r.columns = sorted(r.columns)
-    expected = 2 * [pd.DataFrame(index=idx, columns=sorted(set(col1)), data=1)]
+    result = df.pipe(fin.xs_list)
+    expected = [pd.DataFrame(index=td.idx, columns=td.isin_codes, data=[[p[0], p[2]] for p in td.prices]), pd.DataFrame(index=td.idx, columns=td.isin_codes, data=[[p[1], p[3]] for p in td.prices])]
     for n in range(len(result)):
         assert result[n].all().all() == expected[n].all().all()
 
-    # raises an Exception (no Multiindex)
-    with pytest.raises(Exception):
-        fud.bloomberg_xs(pd.DataFrame(index=idx, columns=sorted(set(col1)), data=1))
+    # raises a NotImplementedError
+    with pytest.raises(NotImplementedError):
+        fin.xs_list(pd.DataFrame(index=td.idx, columns=td.bbg_fields, data=[[p[0], p[1]] for p in td.prices]))
