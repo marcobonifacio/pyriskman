fafc32153a4317d95f8a7adb5adecb8b8b4acdf3
mbonix <bonifacio.marco@gmail.com>
2021-02-14 23:12:46 +0100

    Prices df structure

--- fintopy/prices.py
+++ fintopy/prices.py
@@ -25,10 +25,43 @@ class PricesDataFrameAccessor:
             raise AttributeError('The DataFrame Index must be a DateTime Index.')
         if not df.index.is_unique:
             raise AttributeError('The DataFrame Index cannot have duplicates.')
-        if not df.all().all() >= 0:
+        if not (df > 0).all().all():
             raise AttributeError('The DataFrame cannot have negative prices.')
     
     def log_prices(self):
         """Calculates log prices.
         """
         return(self._df.pipe(np.log))
+        
+    def rebase(self):
+        pass
+    
+    def log_returns(self):
+        pass
+    
+    def pct_returns(self):
+        pass
+    
+    def abs_return(self):
+        pass
+    
+    def annualized_return(self):
+        pass
+    
+    def periodic_returns(self):
+        pass
+    
+    def cagr(self):
+        pass
+    
+    def drawdown(self):
+        pass
+    
+    def max_drawdown(self):
+        pass
+    
+    def comparison_plot(self):
+        pass
+    
+    def drawdown_plot(self):
+        pass
