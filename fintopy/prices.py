40f3b97d9715866de3d548b39358dc34eb12bb6e
Marco Bonifacio <bonifacio.marco@gmail.com>
2021-02-25 15:36:48 +0100

    Completed prices methods

--- fintopy/prices.py
+++ fintopy/prices.py
@@ -8,11 +8,17 @@ import pandas as pd
 @pd.api.extensions.register_series_accessor('prices')
 class PricesSeriesAccessor:
     """Accessor for historical series of financial prices.
-    
+
     Examples:
     >>> s.prices.set_frequency()
     >>> s.prices.rebase()
     >>> s.prices.log_returns()
+    >>> s.prices.pct_returns()
+    >>> s.prices.abs_return()
+    >>> s.prices.annualized_return()
+    >>> s.prices.cagr()
+    >>> s.prices.drawdown()
+    >>> s.prices.max_drawdown()
     """
 
     def __init__(self, series: pd.Series) -> None:
@@ -35,14 +41,14 @@ class PricesSeriesAccessor:
         Args:
             freq: The frequency of the new series. Typical values could be 'B' (Business Day), 'BW' (alias for 'W-FRI', Business Week), 'BM' (Business Month), 'BQ' (Business Quarter), 'BY' (Business Year), Defaults to 'B' (Business Day).
             method: Method fo filling the holes in the reindexed series. Can assume values `None` (fills with NaN), 'pad'/'ffill' (fills with previous value), 'backfill'/'bfill' (fills with next value). Defaults to 'pad'.
-            
+
         Returns:
             pd.Series: A series with a modified frequency.
         """              
         if freq == 'BW':
             freq = 'W-FRI'
         return self._series.asfreq(freq, method)
-    
+
     def rebase(self, base: int = 100) -> pd.Series:
         """Rebases the series.
 
@@ -53,7 +59,7 @@ class PricesSeriesAccessor:
             pd.Series: The rebased series.
         """
         return self._series.divide(self._series.iloc[0]).multiply(base)
-    
+
     def log_returns(self, period: int = 1, dropna: bool = True) -> pd.Series:
         """Calculates logarithmic returns.
 
@@ -68,7 +74,7 @@ class PricesSeriesAccessor:
             return self._series.apply(np.log).diff(period).dropna()
         else:
             return self._series.apply(np.log).diff(period)
-    
+
     def pct_returns(self, period: int = 1, dropna: bool = True) -> pd.Series:
         """Calculates percentage returns.
 
@@ -83,32 +89,52 @@ class PricesSeriesAccessor:
             return self._series.pct_change(period).dropna()
         else:
             return self._series.pct_change(period)
-    
+
     def abs_return(self) -> float:
         """Calculates the absolute return over the series.
 
         Returns:
             float: The absolute return over the series.
         """        
-        return self._series.iat(-1) / self._series.iat(0) - 1
-    
-    def annualized_return(self):
-        pass
-    
-    def periodic_returns(self):
-        pass
-    
-    def cagr(self):
-        pass
-    
-    def drawdown(self):
-        pass
-    
-    def max_drawdown(self):
-        pass
-    
-    def comparison_plot(self):
-        pass
-    
-    def drawdown_plot(self):
-        pass
+        return self._series.iat[-1] / self._series.iat[0] - 1
+
+    def annualized_return(self, base: int = 365) -> float:
+        """Calculates an annualized return over the series.
+
+        Args:
+            base: Number of periods in a year. Defaults to 365.
+
+        Returns:
+            float: The annualized return over the series.
+        """        
+        return self.abs_return() * base /\
+            (self._series.index[-1] - self._series.index[0]).days
+
+    def cagr(self, base: int = 365) -> float:
+        """Calculates a compounded annual growth rate (CAGR).
+
+        Args:
+            base: Number of periods in a year. Defaults to 365.
+
+        Returns:
+            float: The compounded annual growth rate.
+        """        
+        return (1 + self.abs_return()) ** (base / (self._series.index[-1] -\
+            self._series.index[0]).days) - 1
+
+    def drawdown(self) -> pd.Series:
+        """Calculates the drawdown of the series.
+
+        Returns:
+            pd.Series: The drawdown series.
+        """        
+        return self._series.cummax().sub(self._series).\
+            div(self._series.cummax())
+
+    def max_drawdown(self) -> float:
+        """Calculates the max drawdown.
+
+        Returns:
+            float: The max drawdown.
+        """        
+        return self.drawdown().max()
--- tests/test_prices.py
+++ tests/test_prices.py
@@ -30,10 +30,40 @@ def test_validate3(sample_s):  # Fail, duplicate index
         sample_s = sample_s.reindex(idx)
         sample_s.prices.rebase()
 
-def test_rebase(sample_s):  # Pass
-    result = sample_s.prices.rebase(base=200)
-    assert pytest.approx(result.iat[1], 0.0001) == 200.192919
-
 def test_set_frequency(sample_s):  # Pass
     result = sample_s.prices.set_frequency(freq='BW')
     assert result.index[2] == pd.Timestamp('2021-01-22')
+
+def test_rebase(sample_s):  # Pass
+    result = sample_s.prices.rebase(base=200)
+    assert pytest.approx(result.iat[1], 0.0001) == 200.1929
+
+def test_log_returns(sample_s):  # Pass
+    result = sample_s.prices.log_returns()
+    assert pytest.approx(result.iat[3], 0.0001) == 0.006074
+
+def test_pct_returns(sample_s):  # Pass
+    result = sample_s.prices.pct_returns()
+    assert pytest.approx(result.iat[3], 0.0001) == 0.006093
+
+def test_abs_return(sample_s):  # Pass
+    result = sample_s.prices.abs_return()
+    assert pytest.approx(result, 0.0001) == 0.065552
+
+def test_annualized_return(sample_s):  # Pass
+    result = sample_s.prices.annualized_return()
+    assert pytest.approx(result, 0.0001) == 0.957057
+
+def test_cagr(sample_s):  # Pass
+    result = sample_s.prices.cagr()
+    assert pytest.approx(result, 0.0001) == 1.526906
+
+def test_drawdown(sample_s):  # Pass
+    result = sample_s.prices.drawdown()
+    print(result)
+    assert pytest.approx(result.iat[8], 0.0001) == 0.030052
+
+def test_max_drawdown(sample_s):  # Pass
+    result = sample_s.prices.max_drawdown()
+    print(result)
+    assert pytest.approx(result, 0.0001) == 0.031736
